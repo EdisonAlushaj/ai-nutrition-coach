feb20ec438f38597731f497fc577905786f46fbd
Edison <96665518+EdisonAlushaj@users.noreply.github.com>
2026-01-21 20:39:07 +0100

    Update main
    Fixing API Authorization

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -135,7 +135,11 @@ def read_users_me(current_user: models.User = Depends(get_current_user)):
     return current_user
 
 @app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
+def read_user(user_id: int, db: Session = Depends(get_db), current_user: models.User = Depends(get_current_user)):
+    
+    if current_user.id != user_id and current_user.role != models.RoleEnum.admin:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to access this resource")
+
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
